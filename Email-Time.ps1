05b3cacb9f9ad2ef6dfe2db2ad0a06b3f0e58037
trey <trey@fehuit.com>
2018-05-01 15:59:36 -0500

    Trying to unfuck these 2 different git branches

---
+++ Email-Time.ps1
@@ -0,0 +1,6 @@
+﻿$fromEmail = ""
+$errorEmail = ""
+$mailServer = ""
+$currTime = get-date -format F
+ 
+Send-MailMessage -From $fromEmail -to $errorEmail  -subject "Email Time" -SmtpServer $mailServer -body "SCRIPT STARTED AT $currTime"
