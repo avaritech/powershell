1973372bebc89d27ed2fb51dc838088d840bb6ef
trey <trey@fehuit.com>
2018-08-24 08:18:44 -0500

    removed email-time

--- Email-Time.ps1
+++
@@ -1,6 +0,0 @@
-﻿$fromEmail = ""
-$errorEmail = ""
-$mailServer = ""
-$currTime = get-date -format F
- 
-Send-MailMessage -From $fromEmail -to $errorEmail  -subject "Email Time" -SmtpServer $mailServer -body "SCRIPT STARTED AT $currTime"
