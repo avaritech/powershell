5a52b7198983b6f0dc8efa9d35d32587fafa380b
trey <trey@fehuit.com>
2019-03-01 11:24:18 -0600

    updated for multi-IP check

--- hsts-header-check.ps1
+++ hsts-header-check.ps1
@@ -1,8 +1,9 @@
 ﻿#will check for specific header
 #note - dependent on DNS query
 #Charles Smith
-$sites= @("") #input the list of sites to check
-
+$sites = @("") #input the list of sites to check
+$ips = @("")
+$hostheader = ""
 ###############################TRUST ALL CERTS###################################
 add-type @"
     using System.Net;
@@ -19,21 +20,53 @@ add-type @"
 [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
+Function MultiSite(){
+    while($true){
+
+        foreach($site in $sites){
+            $headers = Invoke-WebRequest $site | select Headers
+            #$headers.Headers.Keys 
+            #$headers.Headers.Values
 
 
-foreach($site in $sites){
-    $headers = Invoke-WebRequest $site | select Headers
-    #$headers.Headers.Keys 
-    #$headers.Headers.Values
-
-
-    if($headers.Headers["Strict-Transport-Security"]){
-        write-host $site ":"
-            write-host "`tStrict-Transport-Security header found, value " $headers.Headers["Strict-Transport-Security"] -ForegroundColor Green
-        }
-        else{
-        write-host $site ":"
-        write-host "`tStrict-Transport-Security header not found "  -ForegroundColor RED
+            if($headers.Headers["Strict-Transport-Security"]){
+                write-host $site ":"
+                    write-host "`tStrict-Transport-Security header found, value " $headers.Headers["Strict-Transport-Security"] -ForegroundColor Green
+                }
+                else{
+                write-host $site ":"
+                write-host "`tStrict-Transport-Security header not found "  -ForegroundColor RED
     
+                }
         }
+        start-sleep 5
     }
+}
+
+Function MultiIPs(){
+
+    while($true){
+
+            foreach($IP in $IPs){
+                $headers = Invoke-WebRequest https://$IP/adfs/ls -Headers @{ host=$hostheader } | select Headers
+                #$headers.Headers.Keys 
+                #$headers.Headers.Values
+
+
+                if($headers.Headers["Strict-Transport-Security"]){
+                    write-host $IP ":"
+                        write-host "`tStrict-Transport-Security header found, value " $headers.Headers["Strict-Transport-Security"] -ForegroundColor Green
+                    }
+                    else{
+                    write-host $IP ":"
+                    write-host "`tStrict-Transport-Security header not found "  -ForegroundColor RED
+    
+                    }
+            }
+            start-sleep 5
+        }
+
+}
+
+MultiIPs
+MultiSite #comment out the function you don't want to run. Will run multiIPs by default
